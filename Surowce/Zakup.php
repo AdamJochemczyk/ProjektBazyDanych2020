619681c1049101ffc2ce00d82e6c49a18626a5dc
SebastianSzczypkowski <65536856+SebastianSzczypkowski@users.noreply.github.com>
2020-05-19 11:28:23 +0200

    Add files via upload

---
+++ Surowce/Zakup.php
@@ -0,0 +1,15 @@
+<?php
+$idInwestycje = $_POST["idInwestycje"];
+$Wykupione = $_POST["Wykupione"];
+
+$Server = "localhost";
+$Login = "root";
+$Password = "";
+$Database = "mydb";
+
+$Connection = new mysqli($Server, $Login, $Password, $Database);
+
+$Query = "UPDATE inwestycje SET  Wykupione=1 WHERE idInwestycje=$idInwestycje";
+$Result = $Connection->query($Query);
+header("Location: surowce.php");
+?>
