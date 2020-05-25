16ab1b6df89dd2944a67310e63c22fdac6946fe4
SebastianSzczypkowski <65536856+SebastianSzczypkowski@users.noreply.github.com>
2020-05-25 11:21:55 +0200

    najnowsza baza danych

--- baza_danych/mydb.sql
+++ baza_danych/mydb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 24 Maj 2020, 22:11
+-- Czas generowania: 25 Maj 2020, 11:21
 -- Wersja serwera: 10.4.11-MariaDB
 -- Wersja PHP: 7.4.5
 
@@ -40,24 +40,23 @@ CREATE TABLE `inwestycje` (
 --
 
 INSERT INTO `inwestycje` (`idInwestycje`, `nazwa`, `id_typ`, `koszt_inwestycji`, `Wykupione`) VALUES
-(0, 'Lokata 4% w skali roku', 1, '1000.00', 1),
-(1, 'Łazienki, Warszafka', 2, '99999999.99', 1),
+(0, 'Lokata 4% w skali roku', 1, '1000.00', 0),
+(1, 'Łazienki, Warszafka', 2, '99999999.99', 0),
 (2, 'Złoto', 2, '260000.00', 0),
-(3, '\0Super lokata2.0', 1, '20000.00', 1),
-(4, 'Złoto', 3, '500000.00', 1),
-(5, '\0Platyna', 3, '670000.00', 1),
+(3, '\0Super lokata2.0', 1, '20000.00', 0),
+(4, 'Złoto', 3, '500000.00', 0),
+(5, '\0Platyna', 3, '670000.00', 0),
 (6, 'Dolar Amerykański', 4, '250000.00', 1),
 (7, 'Funt Brytyjski', 4, '160000.00', 1),
 (8, '\0Obligacja wymarzony start', 5, '80000.00', 1),
-(9, 'Obligacje skarbowe', 5, '3200000.00', 1),
 (10, 'Akcje CDRED', 6, '9500000.00', 0),
 (11, 'Akcje ING', 6, '50000.00', 0),
-(12, 'Konto Oszczędnościowe 2.5%', 1, '10000.00', 0),
-(13, 'Lokata Mobilna 2.0%', 1, '25000.00', 0),
-(14, 'Lokata dla nowych klientów 3.0%', 1, '50000.00', 0),
-(15, 'Konto Oszczędnościowe 4%', 1, '100000.00', 0),
-(16, 'Elastyczne konto oszczędnościowe 3.5%', 1, '150000.00', 0),
-(17, 'Lokata roczna', 1, '75000.00', 0),
+(12, 'Konto Oszczędnościowe 2.5%', 1, '10000.00', 1),
+(13, 'Lokata Mobilna 2.0%', 1, '25000.00', 1),
+(14, 'Lokata dla nowych klientów 3.0%', 1, '50000.00', 1),
+(15, 'Konto Oszczędnościowe 4%', 1, '100000.00', 1),
+(16, 'Elastyczne konto oszczędnościowe 3.5%', 1, '150000.00', 1),
+(17, 'Lokata roczna', 1, '75000.00', 1),
 (18, 'Lokata na 10 lat [5% w skali roku]', 1, '250000.00', 1),
 (19, 'Osiedle domków jednorodzinnych Wrocław', 2, '5000000.00', 0),
 (20, 'Dom na półwyspie Suwałki', 2, '1850000.00', 0),
@@ -65,10 +64,10 @@ INSERT INTO `inwestycje` (`idInwestycje`, `nazwa`, `id_typ`, `koszt_inwestycji`,
 (22, 'Nowe domy w stanie developerskim Katowice', 2, '2000000.00', 0),
 (23, 'Wilanowska Crestent Warszawa', 2, '4500000.00', 0),
 (24, 'Karpatia Karpacz ośrodek wypoczynkowy', 2, '6500000.00', 0),
-(25, 'Wisła apartamenty z basenem', 2, '10000000.00', 0),
-(26, 'Działki usługowe przy DK86 w Będzinie', 2, '2500000.00', 0),
+(25, 'Wisła apartamenty z basenem', 2, '10000000.00', 1),
+(26, 'Działki usługowe przy DK86 w Będzinie', 2, '2500000.00', 1),
 (27, 'Stone Hill kurort narciarski', 2, '15000000.00', 0),
-(28, 'Synergy Park Gliwice', 2, '1250000.00', 0),
+(28, 'Synergy Park Gliwice', 2, '1250000.00', 1),
 (29, 'Ropa', 3, '32.74', 0),
 (30, 'Srebro', 3, '590000.00', 0),
 (31, 'Cukier', 3, '222000.00', 0),
@@ -79,14 +78,14 @@ INSERT INTO `inwestycje` (`idInwestycje`, `nazwa`, `id_typ`, `koszt_inwestycji`,
 (36, 'Ołów', 3, '420000.00', 0),
 (37, 'Nikiel', 3, '325000.00', 0),
 (38, 'Cyna', 3, '770000.00', 0),
-(39, 'Euro', 4, '1000000.00', 1),
+(39, 'Euro', 4, '1000000.00', 0),
 (40, 'Dolar australisjki', 4, '500000.00', 1),
 (41, 'real brazylijski', 4, '600000.00', 1),
-(42, 'Juan chiński', 4, '2034500.00', 0),
-(43, 'Kuna chorwacka', 4, '777000.00', 0),
-(44, 'Korona islandzka', 4, '300500.00', 0),
-(45, 'Dolar kanadyjski', 4, '250000.00', 0),
-(46, 'Rubel rosyjski', 4, '3500000.00', 0),
+(42, 'Juan chiński', 4, '2034500.00', 1),
+(43, 'Kuna chorwacka', 4, '777000.00', 1),
+(44, 'Korona islandzka', 4, '300500.00', 1),
+(45, 'Dolar kanadyjski', 4, '250000.00', 1),
+(46, 'Rubel rosyjski', 4, '3500000.00', 1),
 (47, 'Frank szwajcarski', 4, '654000.00', 1),
 (48, 'Lira turecka', 4, '956000.00', 1),
 (49, 'Obligacje 3-miesięczne 0.5%', 5, '100000.00', 0),
@@ -99,7 +98,7 @@ INSERT INTO `inwestycje` (`idInwestycje`, `nazwa`, `id_typ`, `koszt_inwestycji`,
 (56, 'Air France-KLM', 6, '5000000.00', 0),
 (57, 'KGHM Polska Miedź ', 6, '2000000.00', 0),
 (58, 'Banco de Sabadell', 6, '325000.00', 0),
-(59, 'Ryanair', 6, '6500000.00', 0),
+(59, 'Ryanair', 6, '6500000.00', 1),
 (60, 'PKN Orlen', 6, '1230000.00', 1);
 
 -- --------------------------------------------------------
@@ -125,19 +124,26 @@ INSERT INTO `inwestycjeuzytkownik` (`ID_INW`, `idUzytkownik`, `idInwestycje`, `D
 (8, 3, 3, '2020-05-24 19:53:48', NULL),
 (9, 3, 60, '2020-05-24 19:54:04', NULL),
 (12, 5, 40, '2020-05-24 19:56:04', NULL),
-(13, 5, 0, '2020-05-24 19:56:43', NULL),
-(14, 5, 0, '2020-05-24 19:57:13', NULL),
-(15, 5, 0, '2020-05-24 19:57:59', NULL),
-(16, 5, 0, '2020-05-24 19:58:38', NULL),
-(17, 5, 0, '2020-05-24 19:59:56', NULL),
-(18, 5, 0, '2020-05-24 20:00:42', NULL),
-(19, 5, 0, '2020-05-24 20:01:35', NULL),
 (20, 5, 60, '2020-05-24 20:02:33', NULL),
 (21, 5, 60, '2020-05-24 20:02:39', NULL),
 (22, 5, 42, '2020-05-24 20:03:12', NULL),
 (23, 5, 47, '2020-05-24 20:03:58', NULL),
 (24, 5, 47, '2020-05-24 20:04:42', NULL),
-(25, 5, 18, '2020-05-24 20:11:22', NULL);
+(25, 5, 18, '2020-05-24 20:11:22', NULL),
+(26, 5, 17, '2020-05-24 20:13:21', NULL),
+(27, 5, 13, '2020-05-24 20:13:34', NULL),
+(31, 5, 16, '2020-05-24 20:30:49', NULL),
+(32, 5, 12, '2020-05-24 20:31:21', NULL),
+(33, 5, 28, '2020-05-25 06:45:56', NULL),
+(34, 5, 26, '2020-05-25 06:45:57', NULL),
+(44, 6, 44, '2020-05-25 07:25:14', NULL),
+(45, 5, 15, '2020-05-25 08:04:50', NULL),
+(46, 5, 14, '2020-05-25 08:12:58', NULL),
+(47, 5, 38, '2020-05-25 08:19:45', NULL),
+(53, 6, 42, '2020-05-25 08:40:49', NULL),
+(55, 6, 40, '2020-05-25 09:07:39', NULL),
+(56, 6, 25, '2020-05-25 09:12:55', NULL),
+(57, 3, 59, '2020-05-25 09:13:41', NULL);
 
 -- --------------------------------------------------------
 
@@ -193,7 +199,7 @@ CREATE TABLE `uzytkownik` (
   `haslo` varchar(45) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `id_roli` int(11) NOT NULL,
   `wiek` int(11) NOT NULL,
-  `kwota` decimal(10,2) DEFAULT NULL,
+  `kwota` double(20,2) DEFAULT NULL,
   `Imie` varchar(45) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `Nazwisko` varchar(45) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -203,11 +209,11 @@ CREATE TABLE `uzytkownik` (
 --
 
 INSERT INTO `uzytkownik` (`idUzytkownik`, `email`, `haslo`, `id_roli`, `wiek`, `kwota`, `Imie`, `Nazwisko`) VALUES
-(3, 'test@test.pl', 'asdfg123', 2, 21, '10000.00', 'Test', 'Tescikowy'),
-(4, 'rambus2013@aaa.com', 'aasda', 2, 21, '0.00', 'Daw', 'Kan'),
-(5, 'asdf@onet.pl', 'zaq12wsx', 2, 23, '22.00', 'TSES1', 'TSES12'),
-(6, 'tututu@gmail.com', 'Dzekula21', 2, 21, '1000000.00', 'sebix', 'Szczypek'),
-(10, 'admin@admin', 'admin', 1, 33, '1000000.00', 'tututu', 'tututututu');
+(3, 'test@test.pl', 'asdfg123', 2, 21, 300010000.00, 'Test', 'Tescikowy'),
+(4, 'rambus2013@aaa.com', 'aasda', 2, 21, 0.00, 'Daw', 'Kan'),
+(5, 'asdf@onet.pl', 'zaq12wsx', 2, 23, 22.00, 'TSES1', 'TSES12'),
+(6, 'tututu@gmail.com', 'Dzekula21', 2, 21, 1000000000000000000.00, 'sebix', 'Szczypek'),
+(10, 'admin@admin', 'admin', 1, 33, 1000000.00, 'tututu', 'tututututu');
 
 --
 -- Indeksy dla zrzutów tabel
@@ -255,7 +261,7 @@ ALTER TABLE `uzytkownik`
 -- AUTO_INCREMENT dla tabeli `inwestycjeuzytkownik`
 --
 ALTER TABLE `inwestycjeuzytkownik`
-  MODIFY `ID_INW` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+  MODIFY `ID_INW` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=58;
 
 --
 -- AUTO_INCREMENT dla tabeli `uzytkownik`
